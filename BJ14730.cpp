a12a5f18332af4397568b0bffd4f1928c61a1cbf
주영서 <yuuuhooomac@yuuuhooo-macbook.local>
2024-09-25 15:31:03 +0900

    240925 BJ14730 Solve

---
+++ BJ14730.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    double coef[101] = {0};
+    cin >> n;
+    int tmp1, tmp2;
+    int C = 0;
+
+    for(int i=0; i<n; i++) {
+        cin >> tmp1 >> tmp2;
+        coef[i]= tmp1*tmp2;
+    }
+
+    long long sum = 0;
+    for(int i=0; i<n; i++) {
+        sum += coef[i];
+    }
+
+    cout << sum;
+    
+    return 0;
+}
